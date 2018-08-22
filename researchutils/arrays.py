e9f13990560ee27638e9dad4ce58581d0035552c
Yu Ishihara <yuishihara1225@gmail.com>
2018-08-23 08:52:46 +0900

    Add onehot vector generator

--- researchutils/arrays.py
+++ researchutils/arrays.py
@@ -6,12 +6,14 @@ def one_hot(indices,
             on_value=1,
             off_value=0,
             dtype=None):
+    if not len(shape) == 2:
+        raise ValueError('shape must have only rows and columns size(Should be a matrix or vector)')
     if dtype:
         array_dtype = dtype
     else:
         array_dtype = (np.int8 if isinstance(on_value, int)
                        and isinstance(off_value, int) else np.float32)
     array = np.full(shape=shape, fill_value=off_value, dtype=array_dtype)
-    for index in indices:
-        array[index, ...] = on_value
+    for column, row in enumerate(indices):
+        array[row, column] = on_value
     return array
--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -7,16 +7,22 @@ class TestArrays(object):
     def test_one_hot_vector(self):
         one_hot_vector = arrays.one_hot(
             indices=[1], shape=(3, 1), dtype=np.float32)
+        print('one_hot_vector: {}'.format(one_hot_vector))
         assert one_hot_vector.dtype == np.float32
+        assert one_hot_vector[0] == 0
         assert one_hot_vector[1] == 1
 
     def test_one_hot_matrix(self):
         one_hot_vector = arrays.one_hot(
-            indices=[0, 1, 2], shape=(3, 3), dtype=np.int8)
+            indices=[0, 1, 1], shape=(3, 3), dtype=np.int8)
+        print('one_hot_vector: {}'.format(one_hot_vector))
         assert one_hot_vector.dtype == np.int8
         assert one_hot_vector[0][0] == 1
+        assert one_hot_vector[0][1] == 0
         assert one_hot_vector[1][1] == 1
-        assert one_hot_vector[2][2] == 1
+        assert one_hot_vector[1][0] == 0
+        assert one_hot_vector[1][2] == 1
+        assert one_hot_vector[2][0] == 0
 
     def test_one_hot_no_dtype(self):
         one_hot_vector = arrays.one_hot(indices=[0, 1, 2], shape=(3, 3))
@@ -40,6 +46,10 @@ class TestArrays(object):
         assert one_hot_vector[0][0] == 1
         assert one_hot_vector[1][1] == 1
         assert one_hot_vector[2][2] == 1
+
+    def test_one_hot_3x3_tensor(self):
+        with pytest.raises(ValueError):
+            arrays.one_hot(indices=range(9), shape=(3, 3, 3), off_value=0.0)
 
 
 if __name__ == "__main__":
