7cc17484e7da621447b0d64d2db5a257b1c53c12
Yu Ishihara <yuishihara1225@gmail.com>
2018-08-28 22:42:11 +0900

    Add documents for preprocess module

--- researchutils/image/preprocess.py
+++ researchutils/image/preprocess.py
@@ -2,6 +2,19 @@ import numpy as np
 
 
 def compute_mean_image(frames):
+    """
+    Compute mean image of given frames
+
+    Parameters
+    -------
+    frames : list of numpy.ndarray
+        All frame must have same shape
+
+    Returns
+    -------
+    mean_image : numpy.ndarray
+        Mean image of given frame.
+    """
     frame = frames[0]
     prev_frame = frame
     mean_image = np.zeros(shape=frame.shape, dtype=np.float32)
