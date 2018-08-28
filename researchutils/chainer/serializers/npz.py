de880817786cab9f1872659f2da3ba39020190c7
Yu Ishihara <yuishihara1225@gmail.com>
2018-08-28 23:27:17 +0900

    Add save/load model docs

--- researchutils/chainer/serializers/npz.py
+++ researchutils/chainer/serializers/npz.py
@@ -4,12 +4,41 @@ import chainer.serializers
 
 
 def save_model(path, model):
+    """
+    Save model as an npz file to given path
+
+    Parameters
+    -------
+    path : string
+        path of the model to be saved
+    model : chainer.Link
+        model to save parameters
+
+    Raises
+    -------
+    ValueError
+        File already exists
+    """
     if files.file_exists(path):
         raise ValueError('File already exists in {}'.format(path))
     chainer.serializers.save_npz(path, model)
 
 
 def load_model(path, model):
+    """
+    Load model from the npz file of given path
+
+    Parameters
+    ------
+    path : string
+        path of the saved model
+
+    Returns
+    ------
+    model : chainer.Link
+        model with parameters initialized from loaded file
+        if the file does not exist, then will return given model without any changes
+    """
     if not files.file_exists(path):
         return model
     return chainer.serializers.load_npz(path, model)
