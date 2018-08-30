29e180b6fbce7b24d9f11192611705fe5953657c
Yu Ishihara <yuishihara1225@gmail.com>
2018-08-30 21:49:48 +0900

    Add convenient imports

---
+++ researchutils/chainer/iterators/__init__.py
@@ -0,0 +1,2 @@
+from researchutils.chainer.iterators.decorable_multithread_iterator import DecorableMultithreadIterator
+from researchutils.chainer.iterators.decorable_serial_iterator import DecorableSerialIterator
