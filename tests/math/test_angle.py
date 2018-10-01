0383bbab9c7a688b3f54f2892a5b273017a17bfb
Shunichi09 <quick1st97@gmail.com>
2018-10-01 11:36:21 +0900

    add

--- tests/math/test_angle.py
+++ tests/math/test_angle.py
@@ -5,16 +5,16 @@ import random
 
 from researchutils.math.angle import fit_angle_in_range
 
-def test_angle_range_correcter():
+def test_fit_angle_in_range():
     angles = np.random.rand(10) + 10.0
     test_pass = True
     correct_angles = fit_angle_in_range(angles)
 
     for i, angle in enumerate(correct_angles):
-        if angle > math.pi:
+        if angle > 2 * math.pi:
             test_pass = False
             break
-        elif angle < -math.pi:
+        elif angle < 0.0:
             test_pass = False
             break
         elif round(math.cos(float(angle)), 5) != round(math.cos(angles[i]), 5):
@@ -23,18 +23,19 @@ def test_angle_range_correcter():
 
     assert test_pass == True
 
-    angles = np.random.rand(2, 10) + -10.0
+    angles = np.random.rand(3, 10) + -10.0
     correct_angles = fit_angle_in_range(angles)
 
-    for i, angle in enumerate(correct_angles[0]):
-        if angle > math.pi:
-            test_pass = False
-            break
-        elif angle < -math.pi:
-            test_pass = False
-            break
-        elif round(math.cos(float(angle)), 5) != round(math.cos(angles[0, i]), 5):
-            test_pass = False
-            break
+    for j in range(correct_angles.shape[0]):
+        for i, angle in enumerate(correct_angles[j]):
+            if angle > 2 * math.pi:
+                test_pass = False
+                break
+            elif angle < 0.0:
+                test_pass = False
+                break
+            elif round(math.cos(float(angle)), 5) != round(math.cos(angles[j, i]), 5):
+                test_pass = False
+                break
     
     assert test_pass == True
