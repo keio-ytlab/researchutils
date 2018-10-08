d6c1ec5b383142b9a10fd96471e4212d0cae313f
Shunichi09 <quick1st97@gmail.com>
2018-10-08 16:30:21 +0900

    fix the code of angle.py and add new function in angle.py, fit_angle_in_deg_range

--- researchutils/math/angle.py
+++ researchutils/math/angle.py
@@ -36,3 +36,40 @@ def fit_angle_in_range(angles, min_angle=0.0, max_angle=(2 * math.pi)):
 
     output = np.minimum(max_angle, np.maximum(min_angle, output))
     return output.reshape(output_shape)
+
+def fit_angle_in_deg_range(angles, min_angle=0.0, max_angle=360.0):
+    """
+    Check angle range and correct the range
+
+    Parameters
+    -------
+    angle : array-like
+        unit is degrees
+    min_angle : float 
+        maximum of range in degrees, default 0.0
+    max_angle : float 
+        minimum of range in degrees, default 360.0
+
+    Returns
+    -------
+    correct_angle : numpy.ndarray
+        correct range angle in degrees
+    """
+
+    if max_angle < min_angle:
+        raise ValueError("max angle must be greater than min angle")
+    if (max_angle - min_angle) < 360.0:
+        raise ValueError("difference between max_angle and min_angle must be greater than 360.0")
+
+    output = np.array(angles)
+    output_shape = output.shape
+
+    output = output.flatten()
+    output -= min_angle
+    output %= 360.0
+    output += 360.0
+    output %= 360.0
+    output += min_angle
+
+    output = np.minimum(max_angle, np.maximum(min_angle, output))
+    return output.reshape(output_shape)
