01172acf3bc1f5534703eaa48c03c8284a67a3f4
Shunichi09 <quick1st97of@gmail.com>
2018-10-14 22:57:36 +0900

    fix the test_normalization.py's test's functions

--- tests/math/test_normalization.py
+++ tests/math/test_normalization.py
@@ -2,11 +2,12 @@ import pytest
 import random
 import math
 import numpy as np
+
 from researchutils.math.normalization import normalize_by_min_max, standardize
 
-class Testnormalization(object):
+class TestNormalization(object):
     def test_normalize_by_min_max_scalar(self):
-        data = np.random.rand(100) * 3.0 + 2.1
+        data = np.random.randn(100)
         normalized_data = normalize_by_min_max(data)
 
         assert data.shape == normalized_data.shape
@@ -14,11 +15,10 @@ class Testnormalization(object):
         assert np.argmin(data) == np.argmin(normalized_data)
 
         for i in range(len(data)):
-            if normalized_data[i] < 0.0 or normalized_data[i] > 1.0:
-                assert False        
+            assert 0.0 <= normalized_data[i] and normalized_data[i] <= 1.0     
     
     def test_normalize_by_min_max_matrix(self):
-        data = np.random.rand(3, 100) * 3.0 - 2.1
+        data = np.random.randn(2, 100)
         normalized_data = normalize_by_min_max(data)
 
         assert data.shape == normalized_data.shape
@@ -27,11 +27,10 @@ class Testnormalization(object):
 
         for i in range(data.shape[0]):
             for j in range(data.shape[1]):
-                if normalized_data[i, j] < 0.0 or normalized_data[i, j] > 1.0:
-                    assert False
-
+                assert 0.0 <= normalized_data[i, j] and normalized_data[i, j] <= 1.0
+                    
     def test_normalize_by_min_max_diff_range(self):
-        data = np.random.rand(3, 100) * 3.0 - 2.1
+        data = np.random.randn(2, 100)
         normalized_data = normalize_by_min_max(data, min_value=-1.0, max_value=5.0)
 
         assert data.shape == normalized_data.shape
@@ -40,33 +39,36 @@ class Testnormalization(object):
 
         for i in range(data.shape[0]):
             for j in range(data.shape[1]):
-                if normalized_data[i, j] < -1.0 or normalized_data[i, j] > 5.0:
-                    assert False
+                assert -1.0 <= normalized_data[i, j] and normalized_data[i, j] <= 5.0
 
     def test_normalize_by_min_max_wrong_value(self):
         with pytest.raises(ValueError):
-            data = np.random.rand(3, 100) * 3.0 - 2.1
+            data = np.random.randn(2, 100)
             normalized_data = normalize_by_min_max(data, min_value=1.0, max_value=0.0)
     
     def test_standarized_scalar(self):
-        data = np.random.rand(100) * (-5.0) + 1.1
+        test_ave = np.random.randint(-100, 100)
+        test_dev = np.random.randint(100)
+        data = np.random.normal(test_ave, test_dev, size=(100))
         standarized_data = standardize(data)
 
         assert data.shape == standarized_data.shape
         assert np.argmin(data) == np.argmin(standarized_data)
         assert np.argmax(data) == np.argmax(standarized_data)
-        assert round(np.average(standarized_data), 5) == 0.0
-        assert round(np.sqrt(np.var(standarized_data)), 5) == 1.0 
+        assert np.average(standarized_data) == pytest.approx(0.0)
+        assert np.sqrt(np.var(standarized_data)) == pytest.approx(1.0) 
 
     def test_standarized_matrix(self):
-        data = np.random.rand(3, 100) * (-5.0) + 1.1
+        test_ave = np.random.randint(-100, 100)
+        test_dev = np.random.randint(100)
+        data = np.random.normal(test_ave, test_dev, size=(2, 100))
         standarized_data = standardize(data)
 
         assert data.shape == standarized_data.shape
         assert np.argmin(data) == np.argmin(standarized_data)
         assert np.argmax(data) == np.argmax(standarized_data)
-        assert round(np.average(standarized_data), 5) == 0.0
-        assert round(np.sqrt(np.var(standarized_data)), 5) == 1.0
+        assert np.average(standarized_data) == pytest.approx(0.0)
+        assert np.sqrt(np.var(standarized_data)) == pytest.approx(1.0)
 
 if __name__ == '__main__':
     pytest.main()
