363a8a5962b81ea434f494b2e94e857f60ab1936
Yu Ishihara <yu_ishihara.a2@keio.jp>
2018-11-04 17:49:08 +0900

    Add decorable multiprocess iterator

--- researchutils/chainer/iterators/__init__.py
+++ researchutils/chainer/iterators/__init__.py
@@ -1,2 +1,3 @@
+from researchutils.chainer.iterators.decorable_multiprocess_iterator import DecorableMultiprocessIterator
 from researchutils.chainer.iterators.decorable_multithread_iterator import DecorableMultithreadIterator
 from researchutils.chainer.iterators.decorable_serial_iterator import DecorableSerialIterator
